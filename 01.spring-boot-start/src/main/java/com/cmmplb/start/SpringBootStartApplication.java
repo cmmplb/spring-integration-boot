39e1db90a9ce3b8f883d03df8fa56a35b8f6cbf5
penglibo <cmmplb@163.com>
2022-01-06 18:01:42 +0800

    docs(spring-boot-start):添加生成docker镜像配置。

--- 01.spring-boot-start/src/main/java/com/cmmplb/start/SpringBootStartApplication.java
+++ 01.spring-boot-start/src/main/java/com/cmmplb/start/SpringBootStartApplication.java
@@ -1,21 +1,21 @@
 package com.cmmplb.start;
 
 import com.cmmplb.core.utils.SpringApplicationUtil;
-import org.springframework.boot.SpringApplication;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.core.env.ConfigurableEnvironment;
 
 /**
  * @author penglibo
- * @createdate 2021-03-20
+ * @date 2021-03-20
  */
 
+@Slf4j
 @SpringBootApplication
 public class SpringBootStartApplication {
 
     public static void main(String[] args) {
         SpringApplicationUtil.run(SpringBootStartApplication.class, args);
+        System.out.println(log.isDebugEnabled());
     }
 
 }
