c5e707689991c3b251dd68c35913715250e4d25d
penglibo <cmmplb@163.com>
2022-01-18 15:24:14 +0800

    feat(pom):添加pom文件注释。

--- 50.spring-boot-admin/spring-boot-admin-server/src/main/java/com/cmmplb/admin/server/SpringBootAdminServerApplication.java
+++ 50.spring-boot-admin/spring-boot-admin-server/src/main/java/com/cmmplb/admin/server/SpringBootAdminServerApplication.java
@@ -1,19 +1,11 @@
 package com.cmmplb.admin.server;
 
 import com.cmmplb.core.utils.SpringApplicationUtil;
-import de.codecentric.boot.admin.server.config.EnableAdminServer;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.core.env.Environment;
-
-import java.net.InetAddress;
-import java.net.UnknownHostException;
 
 /**
  * @author penglibo
- * @createdate 2021-03-20
+ * @date 2021-03-20
  */
 
 @SpringBootApplication
