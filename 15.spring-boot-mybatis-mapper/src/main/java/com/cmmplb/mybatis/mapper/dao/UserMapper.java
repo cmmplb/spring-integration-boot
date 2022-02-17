d943bb9e1076bbf45bf7b5139ab6b9275b212c97
penglibo <cmmplb@163.com>
2022-02-17 17:36:47 +0800

    feat(spring-boot-mybatis-mapper):添加tk.mybatis模块。

---
+++ 15.spring-boot-mybatis-mapper/src/main/java/com/cmmplb/mybatis/mapper/dao/UserMapper.java
@@ -0,0 +1,68 @@
+package com.cmmplb.mybatis.mapper.dao;
+
+import com.cmmplb.core.beans.QueryPageBean;
+import com.cmmplb.mybatis.mapper.entity.User;
+import com.github.pagehelper.Page;
+import org.apache.ibatis.annotations.Param;
+import tk.mybatis.mapper.common.Mapper;
+
+import java.util.List;
+
+/**
+ * @author penglibo
+ * @date 2021-04-02 00:03:34
+ */
+
+public interface UserMapper extends Mapper<User> {
+
+    /**
+     * 根据id删除用户
+     * @param id 用户id
+     * @return 受影响行数
+     */
+    int deleteById(@Param("id") Long id);
+
+    /**
+     * 根据id修改用户信息
+     * @param user 用户对象
+     * @return 受影响行数
+     */
+    int updateById(User user);
+
+    /**
+     * 获取用户列表
+     * @return 用户列表
+     */
+    List<User> selectList();
+
+    /**
+     * 根据id获取用户信息
+     * @param id 用户id
+     * @return 用户信息
+     */
+    User selectById(@Param("id") Long id);
+
+    /**
+     * 分页查询用户列表
+     * @return
+     */
+    Page<User> selectByPaged();
+
+    /**
+     * 获取分页总条数
+     * @return
+     */
+    Long selectByPagedTotal();
+
+    /**
+     * 获取分页数据列表
+     * @param pageBean
+     * @return
+     */
+    List<User> selectByPagedList(@Param("q") QueryPageBean pageBean);
+
+    /**
+     * 测试当前对象id是否注入
+     */
+    List<User> testCurrentUserId();
+}
---
+++ 30.spring-boot-shiro/src/main/java/com/cmmplb/shiro/general/utils/ShiroUtil.java
@@ -0,0 +1,64 @@
+package com.cmmplb.shiro.general.utils;
+
+import com.cmmplb.core.utils.ObjectUtil;
+import com.cmmplb.shiro.general.beans.UserBean;
+import com.cmmplb.shiro.general.entity.User;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.session.Session;
+import org.apache.shiro.subject.SimplePrincipalCollection;
+import org.apache.shiro.subject.Subject;
+
+/**
+ * @author penglibo
+ * @date 2021-09-27 11:55:50
+ * @since jdk 1.8
+ */
+
+public class ShiroUtil {
+
+    /**
+     * 私有构造器
+     **/
+    private ShiroUtil() {
+    }
+
+    /**
+     * 获取当前 Subject
+     * @return Subject
+     */
+    public static Subject getSubject() {
+        return SecurityUtils.getSubject();
+    }
+
+    /**
+     * 切换身份登录后动态更改subject的用户属性
+     */
+    public static void updateCacheUser(User user) {
+        Subject subject = getSubject();
+        subject.runAs(new SimplePrincipalCollection(user, subject.getPrincipals().getRealmNames().iterator().next()));
+    }
+
+    /**
+     * 获取当前用户Session
+     */
+    public static Session getSession() {
+        return getSubject().getSession();
+    }
+
+    /**
+     * 用户登出
+     */
+    public static void logout() {
+        getSubject().logout();
+    }
+
+    /**
+     * 获取当前用户信息
+     */
+    public static UserBean getUser() {
+        if (getSubject().getPrincipal() instanceof UserBean) {
+            return ObjectUtil.cast(getSubject().getPrincipal());
+        }
+        return null;
+    }
+}
