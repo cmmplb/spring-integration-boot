91ca505638eb56462d8a8b2bfc7b2aa253b3361a
cmmplb <cmmplb@163.com>
2022-06-09 10:48:16 +0800

    feature(start): docker run.sh。

--- 01.spring-boot-start/docker-compose/run.sh
+++ 01.spring-boot-start/docker-compose/run.sh
@@ -1,26 +1,21 @@
 #!/usr/bin/env bash
 
-containerName=spring-boot-start
-image=registry.cn-hangzhou.aliyuncs.com/cmmplb/spring-boot-start:1.0.0
-prot=80:80
-
 function start()
 {
 	echo '=====运行====='
-	docker run -d --restart=always --name ${containerName} -p ${prot} ${image}
+	docker-compose -f docker-compose.yml up -d
 }
 function stop()
 {
 	echo '=====停止====='
-	docker stop ${containerName}
-	docker rm ${containerName}
+	docker-compose -f docker-compose.yml down
 }
 # docker-compose -f docker-compose.yml restart
 function restart()
 {
 	echo '=====重启====='
-	stop
-	start
+	docker-compose -f docker-compose.yml down
+	docker-compose -f docker-compose.yml up -d
 }
 case $1 in
 	"start")
